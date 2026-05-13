6a3144f93849798ebe08ef026b4f3459f7cd762a
“Fran” <Francisco_22_90@hotmail.com>
2026-05-12 23:13:43 -0600

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Mrkz90.github.io', 
+  base: '/Nexa',
   vite: {
     plugins: [tailwindcss()]
   }
